1fa8e47a1b34a7a3e3832eff41602233bba88330
Godwin Naces <godwinnaces@gmail.com>
2024-10-18 16:46:47 +0000

    further checking ang verification of the application error

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3prtk!6*pc5#f%k!472^8wnq(r@hh!h#x)9jw&ins*!sggjpj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-gnaces-djangoblog-tmcue4ynsef.ws.codeinstitute-ide.net','.herokuapp.com']
 
