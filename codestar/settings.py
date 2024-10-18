209bee4ad8bae17315b57064f67c4e25d19d0f8b
Godwin Naces <godwinnaces@gmail.com>
2024-10-18 15:56:46 +0000

    install gunicorn and freeze requirements.txt file, switch Debug = False, append .herokuapp.com

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!3prtk!6*pc5#f%k!472^8wnq(r@hh!h#x)9jw&ins*!sggjpj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-gnaces-djangoblog-tmcue4ynsef.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-gnaces-djangoblog-tmcue4ynsef.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
